9fa92d5ee49357df561da403aa716ae55b14c60e
AF042410 <altamash.fakki@cerner.com>
2017-08-17 12:51:55 -0500

    fixing nightwatch test

--- packages/terra-consumer-icon/tests/nightwatch/icon-spec.js
+++ packages/terra-consumer-icon/tests/nightwatch/icon-spec.js
@@ -19,24 +19,24 @@ module.exports = {
       .expect.element('#icon-default').to.have.attribute('aria-hidden').which.contains('true');
   },
 
-  // 'Displays a default icon with height equal to 2em': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/icon-tests/default`)
-  //     .waitForElementPresent('#icon-default', 1000)
-  //     .expect.element('#icon-default').to.have.attribute('height').which.contains('2em');
-  // },
-  //
-  // 'Displays a default icon with width equal to 2em': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/icon-tests/default`)
-  //     .waitForElementPresent('#icon-default', 1000)
-  //     .expect.element('#icon-default').to.have.attribute('width').which.contains('2em');
-  // },
-  //
-  // 'Displays a default icon with focusable equal to false': (browser) => {
-  //   browser
-  //     .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/icon-tests/default`)
-  //     .waitForElementPresent('#icon-default', 1000)
-  //     .expect.element('#icon-default').to.have.attribute('focusable').which.contains('false');
-  // },
+  'Displays a default icon with height equal to 2em': (browser) => {
+    browser
+      .url(`${browser.launchUrl}/#/tests/icon-tests/default`)
+      .waitForElementPresent('#icon-default', 1000)
+      .expect.element('#icon-default').to.have.attribute('height').which.contains('2em');
+  },
+
+  'Displays a default icon with width equal to 2em': (browser) => {
+    browser
+      .url(`${browser.launchUrl}/#/tests/icon-tests/default`)
+      .waitForElementPresent('#icon-default', 1000)
+      .expect.element('#icon-default').to.have.attribute('width').which.contains('2em');
+  },
+
+  'Displays a default icon with focusable equal to false': (browser) => {
+    browser
+      .url(`${browser.launchUrl}/#/tests/icon-tests/default`)
+      .waitForElementPresent('#icon-default', 1000)
+      .expect.element('#icon-default').to.have.attribute('focusable').which.contains('false');
+  },
 };
