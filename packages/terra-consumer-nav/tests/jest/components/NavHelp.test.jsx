e4e4717a440b1af0a090eb2e6a2c055a53d00bf5
Mahmud Khan <mrkdolon@gmail.com>
2017-08-30 16:47:28 -0400

    Fixed translation issues (#22)
    Fixed translation issues.

--- packages/terra-consumer-nav/tests/jest/components/NavHelp.test.jsx
+++ packages/terra-consumer-nav/tests/jest/components/NavHelp.test.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { I18nProvider } from 'terra-i18n';
 import IconOutlineQuestionMark from 'terra-consumer-icon/lib/icon/IconOutlineQuestionMark';
-import Messages from '../../../src/i18n/translations/messages.json';
+import messages from '../../../translations/en-US.json';
 import NavHelp from '../../../src/components/nav-help/NavHelp';
-
 
 const helpItems = [
   {
@@ -26,7 +25,7 @@ const helpItems = [
 describe('NavHelp button with pop/modal', () => {
   // Snapshot Tests
   it('should render a button with IconInfo,label and a pop/modal not opened', () => {
-    const wrapper = shallow(<I18nProvider locale="en-US" messages={Messages['en-US']}>
+    const wrapper = shallow(<I18nProvider locale="en-US" messages={messages}>
       <NavHelp help={helpItems} id="nav-help-button" /></I18nProvider>);
     expect(wrapper).toMatchSnapshot();
   });
