0ff416f831018a1f61dc395e52543efd737ee928
YZ044636 <yaonan.zhong@cerner.com>
2017-08-31 15:46:21 -0500

    update simple modal

--- packages/terra-consumer-site/src/examples/nav/PortalNav.jsx
+++ packages/terra-consumer-site/src/examples/nav/PortalNav.jsx
@@ -2,6 +2,6 @@
 import React from 'react';
 import Modal from './SimpleModal';
 
-const PortalNav = () => <Modal><h1>This is PortalNav placeholder</h1></Modal>;
+const PortalNav = () => <Modal text="Portal Nav"><h1>This is PortalNav placeholder</h1></Modal>;
 
 export default PortalNav;
--- packages/terra-consumer-site/src/examples/nav/SimpleModal.jsx
+++ packages/terra-consumer-site/src/examples/nav/SimpleModal.jsx
@@ -17,7 +17,7 @@ class SimpleModal extends React.Component {
   render() {
     return (
       <div>
-        <Button text="open modal" onClick={this.handleToggle} />
+        <Button text={this.props.text} onClick={this.handleToggle} />
         {this.state.isOpen &&
           <div style={{ position: 'fixed', left: 0, right: 0, bottom: 0, top: 0, backgroundColor: '#fff' }}>
             <Button variant="link" text="Close" onClick={this.handleToggle} style={{ position: 'fixed', top: 0, right: 0 }} />
@@ -30,6 +30,7 @@ class SimpleModal extends React.Component {
 }
 
 SimpleModal.propTypes = {
+  text: PropTypes.string,
   children: PropTypes.node,
 };
 export default SimpleModal;
