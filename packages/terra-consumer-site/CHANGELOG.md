afd7392d56296f1ddd35d23a28d21614e8108bb7
Yaonan Zhong <zhongyaonan@gmail.com>
2017-09-01 15:49:18 -0500

    Add place holder for nav examples (#25)
    * add place holder for nav examples
    
    * update changelog and simplemodal
    
    * update simple modal

--- packages/terra-consumer-site/CHANGELOG.md
+++ packages/terra-consumer-site/CHANGELOG.md
@@ -2,6 +2,10 @@ Changelog
 =========
 
 
+0.3.1 - (August 31, 2017)
+----------
+Added placeholder for nav example
+
 0.3.0 - (August 22, 2017)
 ----------
 Updated terra-consumer-icons
---
+++ packages/terra-consumer-site/src/examples/nav/SimpleModal.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'terra-button';
+
+class SimpleModal extends React.Component {
+  constructor() {
+    super();
+    this.state = { isOpen: false };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button text={this.props.text} onClick={this.handleToggle} />
+        {this.state.isOpen &&
+          <div style={{ position: 'fixed', left: 0, right: 0, bottom: 0, top: 0, backgroundColor: '#fff' }}>
+            <Button variant="link" text="Close" onClick={this.handleToggle} style={{ position: 'fixed', top: 0, right: 0 }} />
+            {this.props.children}
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+SimpleModal.propTypes = {
+  text: PropTypes.string,
+  children: PropTypes.node,
+};
+export default SimpleModal;
