70f0893d4905686e63cf79df7868208552230606
Mahmud-Khan <mrkdolon@gmail.com>
2017-09-01 16:58:35 -0400

    Added skip-to-main-content anchor

--- packages/terra-consumer-layout/src/Layout.jsx
+++ packages/terra-consumer-layout/src/Layout.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Nav from 'terra-consumer-nav';
 import IconMenu from 'terra-icon/lib/icon/IconMenu';
+import { injectIntl, intlShape } from 'react-intl';
 import styles from './Layout.scss';
 
 const cx = classNames.bind(styles);
@@ -10,6 +11,10 @@ const cx = classNames.bind(styles);
 const propTypes = {
   nav: PropTypes.object.isRequired,
   helpItems: PropTypes.array,
+  /**
+   * Injected react-intl formatting api
+   */
+  intl: intlShape.isRequired,
   children: PropTypes.node.isRequired,
 };
 
@@ -30,21 +35,28 @@ class Layout extends React.Component {
   }
 
   render() {
-    const { nav, helpItems, ...customProps } = this.props;
+    const { nav, helpItems, intl, ...customProps } = this.props;
     return (
-      <div className={cx('layout', customProps.className)} {...customProps}>
-        <div className={cx('nav-container')}>
-          <Nav
-            {...nav}
-            isMobileNavOpen={this.state.isMobileNavOpen}
-            onRequestClose={this.toggleNav}
-          />
+      <div>
+        <div className={cx('skip-container')}>
+          <a className={cx('skip-to-main-content')} href="#main-container" id="skip-maincontent-link">
+            {intl.formatMessage({ id: 'Terra.Consumer.Layout.skipcontent' })}
+          </a>
         </div>
-        <main className={cx('main-container')}>
-          <button className={cx('nav-burger')} onClick={this.toggleNav}><IconMenu /></button>
-          {this.props.children}
-          <Nav.Help className={cx('help-button')} helpNavs={helpItems} id="nav-help-button" />
-        </main>
+        <div className={cx('layout', customProps.className)} {...customProps}>
+          <div className={cx('nav-container')}>
+            <Nav
+              {...nav}
+              isMobileNavOpen={this.state.isMobileNavOpen}
+              onRequestClose={this.toggleNav}
+            />
+          </div>
+          <main id="main-container" className={cx('main-container')}>
+            <button className={cx('nav-burger')} onClick={this.toggleNav}><IconMenu /></button>
+            {this.props.children}
+            <Nav.Help className={cx('help-button')} helpNavs={helpItems} id="nav-help-button" />
+          </main>
+        </div>
       </div>
     );
   }
@@ -52,4 +64,4 @@ class Layout extends React.Component {
 
 Layout.propTypes = propTypes;
 
-export default Layout;
+export default injectIntl(Layout);
