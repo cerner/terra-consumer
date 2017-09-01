824e2ae1a7013fdfe1e82e0d3fb077d8917f9ed8
Joseph Fortebuono <Joseph.Fortebuono@cerner.com>
2017-09-01 09:09:35 -0400

    added nav link to helper menu example

--- packages/terra-consumer-nav/tests/nightwatch/PortalNav.jsx
+++ packages/terra-consumer-nav/tests/nightwatch/PortalNav.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import IconPerson from 'terra-icon/lib/icon/IconPerson';
 // NOTE: swap these for deploy/development
 // import Nav from '../../lib/Nav';
@@ -9,14 +10,15 @@ const PortalNav = () => {
   const props = {
     nav: {
       navItems: [
+
         {
+
           // any link that doesn't use react router link is external
           isExternal: true,
           url: 'localhost:8080',
           text: 'Home',
         },
         {
-          isExternal: true,
           url: '/',
           text: 'Dashboard',
           icon: <IconPerson />,
@@ -99,20 +101,117 @@ const PortalNav = () => {
         isCard: true,
       },
       profile: {
-        profileLinks: [
+        profileLinks:
+        [
           {
-            isExternal: true,
+            badgeValue: null,
+            attrs: null,
+            target: '_self',
+            url: 'http://localhost:8080/',
             text: 'Account',
-            icon: (<IconOutlineQuestionMark />),
-            url: 'http://localhost:8080/',
+            subItems: [],
+            isActive: false,
+            isExternal: true,
+            icon: 'icon-cog',
           },
           {
-            isExternal: true,
-            text: 'Notifications',
-            icon: (<IconOutlineQuestionMark />),
+            badgeValue: null,
+            attrs: null,
+            target: '_self',
             url: 'http://localhost:8080/',
+            text: 'Notifications',
+            subItems: [],
+            isActive: false,
+            isExternal: true,
+            icon: 'icon-message-send',
           },
-        ],
+          {
+            badgeValue: null,
+            attrs: null,
+            target: '_self',
+            url: 'http://localhost:8080/',
+            text: 'Patient Information',
+            subItems: [],
+            isActive: false,
+            isExternal: true,
+            icon: 'icon-demographics',
+          },
+          {
+            badgeValue: null,
+            attrs: null,
+            target: '_self',
+            url: 'http://localhost:8080/',
+            text: 'Access Logs',
+            subItems: [],
+            isActive: false,
+            isExternal: true,
+            icon: 'icon-client-access-logs',
+          },
+          {
+            badgeValue: null,
+            attrs: null,
+            target: '_blank',
+            url: 'http://localhost:8080/',
+            text: 'Help',
+            subItems: [],
+            isActive: false,
+            isExternal: true,
+            icon: 'icon-client-question-alt',
+          },
+          {
+            badgeValue: null,
+            attrs: null,
+            target: '_self',
+            url: '#',
+            isExternal: true,
+            text: 'English (United States)',
+            subItems:
+            [{
+              badgeValue: null,
+              attrs: { 'data-locale': 'ar' },
+              target: '_self',
+              url: '#',
+              text: '&#8235;\u0627\u0644\u0639\u0631\u0628\u064a\u0651\u0629&#8236;',
+              subItems: [],
+              isActive: false,
+              isExternal: true,
+              icon: null,
+            },
+            {
+              badgeValue: null,
+              attrs: { 'data-locale': 'es' },
+              target: '_self',
+              url: '#',
+              text: '&#8234;Espa\u00f1ol&#8236;',
+              subItems: [],
+              isActive: false,
+              isExternal: true,
+              icon: null,
+            },
+            {
+              badgeValue: null,
+              attrs: { 'data-locale': 'en-gb' },
+              target: '_self',
+              url: '#',
+              text: '&#8234;English (United Kingdom)&#8236;',
+              subItems: [],
+              isActive: false,
+              isExternal: true,
+              icon: null,
+            },
+            {
+              badgeValue: null,
+              attrs: { 'data-locale': 'fr-fr' },
+              target: '_self',
+              url: '#',
+              text: '&#8234;Fran\u00e7ais (France)&#8236;',
+              subItems: [],
+              isActive: false,
+              isExternal: true,
+              icon: null,
+            }],
+            isActive: false,
+            icon: null }],
         // comment out userName to see signin
         userName: 'John Snow',
         signinUrl: 'http://localhost:8080/',
@@ -146,12 +245,23 @@ const PortalNav = () => {
         text: 'Another link',
         url: 'http://localhost:8080/',
       },
+      {
+        text: 'Nav Link',
+        url: '/test',
+      },
     ],
     locale: 'en-US',
   };
-
+  const content = (
+    <div>
+      <Route exact path="/" render={() => <h1>Home, sweet home!</h1>} />
+      <Route exact path="/test" render={() => <h1>Test Link! Works</h1>} />
+    </div>
+    );
   return (
-    <Layout {...props} id="layout" mainContent="This is portal content" />
+    <BrowserRouter>
+      <Layout {...props} id="layout" mainContent={content} />
+    </BrowserRouter>
   );
 };
 
