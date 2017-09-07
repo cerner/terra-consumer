3630e60cb7942d202d57194d675aafaff5c11cc1
Mahmud-Khan <mrkdolon@gmail.com>
2017-09-07 13:01:07 -0400

    Modified based on code review comments

--- packages/terra-consumer-layout/src/Layout.jsx
+++ packages/terra-consumer-layout/src/Layout.jsx
@@ -56,7 +56,7 @@ class Layout extends React.Component {
           />
           <main id="main-container" className={cx('main-container', this.state.isMobileNavOpen && 'nav-open')}>
             <Nav.Burger className={cx('nav-burger')} handleClick={this.toggleNav} />
-            {this.props.children}
+            <div className={cx('main-content')}>{this.props.children}</div>
             <Nav.Help className={cx('help-button')} helpNavs={helpItems} id="nav-help-button" />
           </main>
         </div>
