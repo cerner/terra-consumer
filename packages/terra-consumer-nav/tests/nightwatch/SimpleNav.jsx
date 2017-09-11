8c18934c8dc6c9e749368ad2663332b1630fb38c
Kevin Jackson <kevin.jackson@cerner.com>
2017-09-11 11:51:33 -0400

    fixing lint issues

--- packages/terra-consumer-nav/tests/nightwatch/SimpleNav.jsx
+++ packages/terra-consumer-nav/tests/nightwatch/SimpleNav.jsx
@@ -63,6 +63,7 @@ const nav = {
           url: '#inbox',
           text: 'An Active Link',
           badgeValue: 1,
+          isActive: true,
         },
       ],
     },
@@ -203,6 +204,8 @@ const nav = {
 export default () => (
   <I18nShell>
     {/* eslint-disable no-alert */}
-    <Nav {...nav} isMobileNavOpen onRequestClose={() => alert('Callback to close')} id="nav" />
+    <div style={{ width: '320px' }}>
+      <Nav {...nav} onRequestClose={() => alert('Callback to close')} id="nav" />
+    </div>
   </I18nShell>
 );
