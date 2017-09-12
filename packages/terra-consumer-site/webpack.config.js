bb96a610ab9e1ac5f9df68afbce4447be29aa194
AF042410 <altamash.fakki@cerner.com>
2017-09-12 11:15:22 -0500

    settingprofile-link-border-background to color instead of image url

--- packages/terra-consumer-site/webpack.config.js
+++ packages/terra-consumer-site/webpack.config.js
@@ -23,6 +23,8 @@ customProperties.setVariables({
   '--terra-consumer-nav-text-color': '#3d3d3d',
   '--terra-consumer-mobile-close-button-color': '#3d3d3d',
   // profile
+  // TODO profile-link-border-background color is time being as we don't have best way to access background image
+  '--terra-consumer-profile-link-border-background': '#c7d4ea',
   '--terra-consumer-profile-link-background': 'rgba(255, 255, 255, 0.25)',
   '--terra-consumer-profile-link-width': '320px',
   '--terra-consumer-profile-link-color': '#3d3d3d',
