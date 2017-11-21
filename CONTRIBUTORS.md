c31a3f7744394e9bae893c5d2386a49f7d6614fc
Jim Pruetting <jim.pruetting@cerner.com>
2017-11-21 13:08:31 -0600

    Add custom callback to profile links for click events

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -4,8 +4,9 @@ Cerner Corporation
 - Kevin Jackson [@KevinJJackson]
 - Ryan Manuel [@ryanthemanuel]
 - Altamash Fakki [@amf9t2]
-- Muhammad M Khan [@Mahmud-Khan] 
+- Muhammad M Khan [@Mahmud-Khan]
 - Alex Bezek [@alex-bezek]
+- Jim Pruetting [@jpruetting]
 
 [@mhemesath]: https://github.com/mhemesath
 [@KevinJJackson]: https://github.com/KevinJJackson
@@ -13,3 +14,4 @@ Cerner Corporation
 [@alex-bezek]: https://github.com/alex-bezek
 [@amf9t2]: https://github.com/amf9t2
 [@Mahmud-Khan]:https://github.com/Mahmud-Khan
+[@jpruetting]:https://github.com/jpruetting
--- packages/terra-consumer-nav/tests/jest/components/ProfileLinks.test.jsx
+++ packages/terra-consumer-nav/tests/jest/components/ProfileLinks.test.jsx
@@ -13,6 +13,33 @@ const testData = [
     icon: (<IconOutlineQuestionMark height={16} width={16} />),
     url: 'http://localhost:8080/',
   },
+  {
+    isExternal: true,
+    url: '#',
+    text: 'English (United States)',
+    subItems: [
+      {
+        'data-locale': 'ar',
+        text: '\u0627\u0644\u0639\u0631\u0628\u064a\u0651\u0629',
+        customOnClick: customOnClickMock,
+      },
+      {
+        'data-locale': 'es',
+        text: 'Espa\u00f1ol',
+        customOnClick: customOnClickMock,
+      },
+      {
+        'data-locale': 'en-gb',
+        text: 'English (United Kingdom)',
+        customOnClick: customOnClickMock,
+      },
+      {
+        'data-locale': 'fr-fr',
+        text: 'Fran\u00e7ais (France)',
+        customOnClick: customOnClickMock,
+      },
+    ],
+  },
 ];
 
 describe('Profile Link items', () => {
